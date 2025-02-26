97b3a4f901e7fe6c22ff070dcaed63c4c6af8e16
Luis Salas <luix_eduard@hotmail.com>
2025-02-26 09:41:23 -0600

    add author

--- src/stories/Datatable.tsx
+++ src/stories/Datatable.tsx
@@ -5,7 +5,7 @@ import { v4 } from "uuid";
 import { format, FormatOptions } from "date-fns";
 
 /**
- * @author @luixeduard
+ * @author Luis Salas @luixeduard
  * @version 2.0.0
  * @name Datatable
  */
